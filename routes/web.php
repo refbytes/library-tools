66a2aba85a4ec48f7ce9ad1adf8b2d0cd6d5286a
Jon Fackrell <jon.fackrell@gmail.com>
2024-08-23 10:37:59 -0600

    wip
    - removed default welcome page which allows a-z list to be the default

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::middleware('signed')
     ->get('invitation/{invitation}/accept', \App\Livewire\AcceptInvitation::class)
